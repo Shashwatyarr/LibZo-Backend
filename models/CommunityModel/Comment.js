16ba546f6f05c3f153496c6af9fe087912eeae6d
Shashwat Srivastava <shashwat.2428it2297@kiet.edu>
2026-02-18 10:21:29 +0530

    club comment fixed

--- models/CommunityModel/Comment.js
+++ models/CommunityModel/Comment.js
@@ -6,7 +6,7 @@ const PostCommentSchema = new mongoose.Schema(
       type: mongoose.Schema.Types.ObjectId,
       ref: "ClubPost",
     },
-    userID: {
+    userId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
     },
